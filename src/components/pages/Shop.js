d663b05c317381d7cb32869767b7de090c031d25
hamid-murambiwa <machipisajunior@gmail.com>
2022-07-10 22:20:15 +0200

    Fix nav and add review feature

--- src/components/pages/Shop.js
+++ src/components/pages/Shop.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { CartContext } from "../CartContext";
+import { Menu, MenuItem, MenuButton } from '@szhsin/react-menu';
+import '@szhsin/react-menu/dist/core.css';
 import img from "../../styling/images/shopping-cart.png";
 import "../../styling/shop.css";
 
@@ -13,9 +15,33 @@ function Shop() {
         <div>
             <section id="shop-container">
                 <div className="s-b-con">
+                    <header className="mobile-header">
+                    <Menu className='menu-icon' menuButton={<MenuButton>&#9776;</MenuButton>}>
+                        <MenuItem><a href="/">Home</a></MenuItem>
+                        <MenuItem><a href="/shop">Shop</a></MenuItem>
+                        <div class="btn-group dropend">
+                        <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                            Furniture Categories
+                        </button>
+                        <ul class="dropdown-menu">
+                            <Link to="livingRoom">LIVING ROOM</Link>
+                            <Link to="diningKitchen">DINING & KITCHEN</Link>
+                            <Link to="bedroom">BEDROOM</Link>
+                            <Link to="storageMedia">STORAGE & MEDIA</Link>
+                            <Link to="office">OFFICE</Link>
+                        </ul>
+                        </div>
+                        <MenuItem><a href="/about">About</a></MenuItem>
+                        <MenuItem><a href="/contact">Contact</a></MenuItem>
+                    </Menu>
+                    </header>
                     <div className="home-btn">
                         <Link to="/"><h2>Strut</h2></Link>
                     </div>
+                    <nav className="s-nav">
+                        <a href="/about">About</a>
+                        <a href="/contact">Contact</a>
+                    </nav>
                     <Link to="/shop/cart" className="cart-btn">
                         <img src={img} alt="cart icon" />
                         <span className="cart-counter">{cart.length}</span>
