a7dabffcf9f279d323b2a459dbb2c30c929696be
hamid-murambiwa <machipisajunior@gmail.com>
2022-07-10 22:20:56 +0200

    Create a functions to handle review api calls

--- src/services/services.js
+++ src/services/services.js
@@ -8,9 +8,71 @@ export const fetchAllCategories = async () => {
   return categoriesData;
 };
 
-// GET ALL fruniture
+// Reviews
 export const fetchAllFurniture = async (categoryId) => {
   const furnitureData = await axios.get(`${BACK_END_URL}/categories/${categoryId}/furniture_items`)
     .then((res) => res.data);
   return furnitureData;
+};
+
+// GET ALL Reviews
+export const fetchAllReview = async (categoryId, reviewId) => {
+  const reviewData = await axios.get(`${BACK_END_URL}/categories/${categoryId}//furniture_items/${reviewId}/review_items`)
+    .then((res) => res.data);
+  return reviewData;
+};
+
+export function componentDidMount(findId, Page, averageRating, reviews, setReviews, setMessage, message, setAverageRating) {
+
+  fetch(`http://localhost:3000/api/v1/categories/${findId()}/furniture_items/${Page()}/reviews`)
+      .then(async response => {
+          const data = await response.json();
+          reviews = data;
+          setReviews(data);
+          handleAverage(reviews, averageRating, setAverageRating);
+          
+          if (!response.ok) {
+              const error = (data && data.message) || response.statusText;
+              return Promise.reject(error);
+          }
+      })
+      .catch(error => {
+          setMessage(message);
+      });
+}
+
+export function handleAverage(reviews, averageRating, setAverageRating) {
+  console.log(reviews[0]["id"])
+
+  if (reviews[0]["id"] > 0) {
+      setAverageRating(reviews.reduce((acc, curr) => acc + curr.overal_rating, 0) / reviews.length);
+      console.log(averageRating);
+  }
+}
+
+export let handleReview = async (rating, title, reviewDes, Page, findId, setTitle, setReviewDes, setMessage) => {
+  const data = {
+      overal_rating: rating,
+      title: title,
+      description: reviewDes,
+      furniture_item_id: Page()
+    };
+  try {
+    let res = await fetch(`http://localhost:3000/api/v1/categories/${findId()}/furniture_items/${Page()}/reviews`, {
+      method: "POST",
+      headers: {
+          'Content-Type': 'application/json'
+          },
+      body: JSON.stringify(data),
+    });
+    if (res.status === 200) {
+      setTitle("");
+      setReviewDes("");
+      setMessage("User created successfully");
+    } else {
+      setMessage("Some error occured");
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
